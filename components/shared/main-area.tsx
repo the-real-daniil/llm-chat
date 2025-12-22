189e63ff882979ef2b770b023befbdc91300e895
Кирилл С. <cllaws2121@gmail.com>
2025-12-22 16:42:49 +0300

    Верстка main-area

---
+++ components/shared/main-area.tsx
@@ -0,0 +1,16 @@
+'use client';
+import MainEmptyState from './main-empty-state';
+import MainHeader from './main-header';
+
+
+const MainArea = () => {
+
+  return (
+    <div className="flex-1 bg-white border border-solid border-gray-300 rounded-2xl shadow-sm">
+      <MainHeader />
+
+      <MainEmptyState />
+    </div>
+  );
+};
+export default MainArea;
